29f91b4eb81e9e19386ff3bd6a1c78ae6ccc1b1f
Nick <b10303008@gmail.com>
2019-03-25 22:03:58 +0800

    fix 倒數日推播

--- app/Services/DynamicNotification.php
+++ app/Services/DynamicNotification.php
@@ -16,9 +16,9 @@ class DynamicNotification
         ])->exists()){
             return False;
         };
-        if($today->gt($start_date) && $end_date->gt($today)){
+        if($start_date->lessThanOrEqualTo($today) && $today->lessThanOrEqualTo($end_date)){
             $count_start = $today->diffInDays($start_date) + 1;
-            $count_end = $today->diffInDays($end_date);
+            $count_end = $today->diffInDays($end_date) + 1;
             $msg = '早安冠樺，今天是阿宇當兵的第 '.$count_start.'天，也是距離我們養貓計畫倒數的第 '.$count_end.' 天哦！';
             PushList::create([
                 'push_schedule_id' => 0,
