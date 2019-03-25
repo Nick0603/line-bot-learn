4bf9a164b1b9d0d22a58116e64ea075ae0d7fbab
Nick <b10303008@gmail.com>
2019-03-25 19:59:41 +0800

    倒數天數推播

--- app/Services/DynamicNotification.php
+++ app/Services/DynamicNotification.php
@@ -6,18 +6,20 @@ use App\PushList;
 class DynamicNotification
 {
     public function createCountEndDateNoficaition(){
-        $end_date = Carbon::parse('2019/3/26');
+        $start_date = Carbon::parse('2019/3/26');
+        $end_date = Carbon::parse('2019/7/25');
         $today = Carbon::now()->startOfDay();
         if( PushList::where([
             ['push_at','>',Carbon::now()->startOfDay()],
             ['push_line_id','1'],
-            ['push_msg','like','距離養貓計畫開始日還有%']
+            ['push_msg','like','早安冠樺，今天是阿宇當兵的第%']
         ])->exists()){
             return False;
         };
-        if($end_date->gt($today)){
-            $count = $today->diffInDays($end_date);
-            $msg = '距離養貓計畫開始日還有 '.$count.' 天哦！';
+        if($today->gt($start_date) && $end_date->gt($today)){
+            $count_start = $today->diffInDays($start_date) + 1;
+            $count_end = $today->diffInDays($end_date);
+            $msg = '早安冠樺，今天是阿宇當兵的第 '.$count_start.'天，也是距離我們養貓計畫倒數的第 '.$count_end.' 天哦！';
             PushList::create([
                 'push_schedule_id' => 0,
                 'push_line_id' => 1,
